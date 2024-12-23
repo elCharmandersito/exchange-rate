c61771a14ad2b23ee1edfa019c94c0d70997998e
elcharmandersito <patriciorobertoparrasoto@gmail.com>
2024-12-23 17:56:56 -0300

    wip

--- src/main/java/com/ppsoto/divisaexchange/controller/ExchangeController.java
+++ src/main/java/com/ppsoto/divisaexchange/controller/ExchangeController.java
@@ -1,14 +1,29 @@
 package com.ppsoto.divisaexchange.controller;
 
+import com.ppsoto.divisaexchange.service.ExchangeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(name = "/exchange")
+@RequestMapping(value = "/exchange")
 @CrossOrigin
 public class ExchangeController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExchangeController.class);
+
+    @Autowired
+    private ExchangeService exchangeService;
+
+    @GetMapping(value = "/{countryCode}", produces = "application/json")
+    public ResponseEntity<String> getExchangeForCountry(
+            @PathVariable(name = "countryCode") String countryCode
+    ) {
+        LOGGER.info("Getting exhcange for : ", countryCode);
+
+        String response = exchangeService.getExchangeForCountry(countryCode);
+
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/ppsoto/divisaexchange/service/ExchangeService.java
+++ src/main/java/com/ppsoto/divisaexchange/service/ExchangeService.java
@@ -7,4 +7,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class ExchangeService {
     private static final Logger LOOGER = LoggerFactory.getLogger(ExchangeService.class);
+
+    public String getExchangeForCountry(String countryCode) {
+        LOOGER.info("Getting exchange for country code :", countryCode);
+        return countryCode;
+    }
 }
