1f0136bda2f7ddb71d093d7f302973b7448bd833
elcharmandersito <patriciorobertoparrasoto@gmail.com>
2024-12-23 17:28:49 -0300

    Update Controller

--- src/main/java/com/ppsoto/divisaexchange/controller/ExchangeController.java
+++ src/main/java/com/ppsoto/divisaexchange/controller/ExchangeController.java
@@ -1,7 +1,14 @@
 package com.ppsoto.divisaexchange.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/exchange")
+@RestController
+@RequestMapping(name = "/exchange")
+@CrossOrigin
 public class ExchangeController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExchangeController.class);
 }
